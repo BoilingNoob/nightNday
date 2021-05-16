28bc171456711f39cffd3caade92f26b11d353ff
dasdot <dealwithit441@gmail.com>
2021-05-16 15:53:48 -0400

    improved scalability and added get-recird functionality

--- nightNday.ps1
+++ nightNday.ps1
@@ -1,6 +1,14 @@
-function get-record($path = 'record.txt'){
+function get-record($path = '.\record.txt'){
     $fileRecord = Get-content $path
-    return $fileRecord
+    if($fileRecord -eq "Day"){
+        return "Day"
+    }
+    elseif($fileRecord -eq "Night"){
+        return "Night"
+    }
+    else{
+        return "No Record"
+    }
 }
 #$regPath = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount\$$windows.data.bluelightreduction.settings\Current'
 #$nightSetting = ([byte[]](2,0,0,0,199,91,231,198,81,107,210,1,0,0,0,0,67,66,1,0,2,1,202,20,14,21,0,202,30,14,7,0,207,40,208,15,202,50,14,16,46,49,0,202,60,14,8,46,47,0,0))
@@ -29,17 +37,33 @@ function set-nightlight(){
         Write-Error "No affect made"
     }
 }
-function set-NightMode($baseData){
+function set-NightMode($recordPath = '.\record.txt'){
     #$nightSetting = ([byte[]](2,0,0,0,199,91,231,198,81,107,210,1,0,0,0,0,67,66,1,0,2,1,202,20,14,21,0,202,30,14,7,0,207,40,208,15,202,50,14,16,46,49,0,202,60,14,8,46,47,0,0))
-    Start-Process -FilePath C:\Users\LargeQuinzhee\Desktop\NightAndDay\night.bat -WindowStyle Hidden
-    set-nightlight -mode Night
+    Start-Process -FilePath '.\night.bat' -WindowStyle Hidden
+    set-nightlight -mode 'Night'
     Invoke-Item "C:\Users\LargeQuinzhee\Desktop\dimmer\Dimmer.exe"
+    Set-Content -Path $recordPath -Value "Night"
 }
-function set-DayMode($baseData){
+function set-DayMode($recordPath = '.\record.txt'){
     #$baseData.daySetting = ([byte[]](0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
-    Start-Process -FilePath C:\Users\LargeQuinzhee\Desktop\NightAndDay\day.bat -WindowStyle Hidden
-    set-nightlight -mode Day
+    Start-Process -FilePath '.\day.bat' -WindowStyle Hidden
+    set-nightlight -mode 'Day'
     get-process -Name 'Dimmer'|Where-Object{$_.Path -eq "C:\Users\LargeQuinzhee\Desktop\dimmer\Dimmer.exe"}|Stop-Process
+    Set-Content -Path $recordPath -Value "Day"
+}
+
+function switch-mode($recordPath = '.\record.txt'){
+    $record = get-record
+
+    if($record -eq "Day"){
+        set-NightMode -recordPath $recordPath
+    }
+    elseif($record -eq "Night"){
+        set-DayMode -recordPath $recordPath
+    }
+    else{
+        set-NightMode -recordPath $recordPath
+    }
 }
 
 #set-nightlight -mode Night

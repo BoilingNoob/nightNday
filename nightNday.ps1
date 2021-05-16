d40ff6f53868e2a5737021f81fdd420418d392b1
dasdot <dealwithit441@gmail.com>
2021-05-16 16:07:08 -0400

    cleaned formatting and added returns to nightnday mode

--- nightNday.ps1
+++ nightNday.ps1
@@ -1,10 +1,10 @@
 function get-record($path = '.\record.txt'){
     $fileRecord = Get-content $path
-    if($fileRecord -eq "Day"){
-        return "Day"
+    if($fileRecord -eq "day"){
+        return "day"
     }
-    elseif($fileRecord -eq "Night"){
-        return "Night"
+    elseif($fileRecord -eq "night"){
+        return "night"
     }
     else{
         return "No Record"
@@ -15,12 +15,9 @@ function get-record($path = '.\record.txt'){
 #$daySetting = ([byte[]](0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
 function set-nightlight(){
     param(
-        [ValidateSet("Day", "Night","day", "night")] $mode = 'Day'
+        [ValidateSet("day","night")] $mode = 'day'
     )
-    $mode = $mode.ToLower()
-
     $editingName = 'Data'
-    
     $baseData = @{
         #RegPath = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount\$$windows.data.bluelightreduction.settings\Current'
         RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount\$$windows.data.bluelightreduction.bluelightreductionstate\Current'
@@ -33,44 +30,40 @@ function set-nightlight(){
     elseif($mode -eq 'day'){
         Set-ItemProperty -Path $baseData.regPath -Name $editingName -Value $baseData.daySetting
     }
-    else{
-        Write-Error "No affect made"
-    }
+    else{Write-Error "No affect made"}
 }
 function set-NightMode($recordPath = '.\record.txt'){
     #$nightSetting = ([byte[]](2,0,0,0,199,91,231,198,81,107,210,1,0,0,0,0,67,66,1,0,2,1,202,20,14,21,0,202,30,14,7,0,207,40,208,15,202,50,14,16,46,49,0,202,60,14,8,46,47,0,0))
     Start-Process -FilePath '.\night.bat' -WindowStyle Hidden
-    set-nightlight -mode 'Night'
+    set-nightlight -mode 'night'
     Invoke-Item "C:\Users\LargeQuinzhee\Desktop\dimmer\Dimmer.exe"
-    Set-Content -Path $recordPath -Value "Night"
+    Set-Content -Path $recordPath -Value "night"
 }
 function set-DayMode($recordPath = '.\record.txt'){
     #$baseData.daySetting = ([byte[]](0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
     Start-Process -FilePath '.\day.bat' -WindowStyle Hidden
-    set-nightlight -mode 'Day'
+    set-nightlight -mode 'day'
     get-process -Name 'Dimmer'|Where-Object{$_.Path -eq "C:\Users\LargeQuinzhee\Desktop\dimmer\Dimmer.exe"}|Stop-Process
-    Set-Content -Path $recordPath -Value "Day"
+    Set-Content -Path $recordPath -Value "day"
 }
 function switch-mode($recordPath = '.\record.txt'){
-    $record = get-record
+    $record = get-record -path $recordPath
 
-    if($record -eq "Day"){
+    if($record -eq "day"){
         set-NightMode -recordPath $recordPath
+        return "night"
     }
-    elseif($record -eq "Night"){
+    elseif($record -eq "night"){
         set-DayMode -recordPath $recordPath
+        return "day"
     }
     else{
         set-NightMode -recordPath $recordPath
+        return 'night'
     }
 }
-
 #set-nightlight -mode Night
 #$record = get-record
-
 #$record
-
 #set-NightMode $baseData
 #set-DayMode
-
-get-record

7f2857abcd47d7c2c034553ff6c3ada2d087df1e
dasdot <dealwithit441@gmail.com>
2021-06-17 00:10:43 -0400

    moved functions to module and added module import

--- nightNday.ps1
+++ nightNday.ps1
@@ -1,73 +1,29 @@
-function get-record($path = '.\ancillary_files\record.txt'){
-    $fileRecord = Get-content $path
-    if($fileRecord -eq "day"){
-        return "day"
-    }
-    elseif($fileRecord -eq "night"){
-        return "night"
-    }
-    else{
-        return "No Record"
+Import-Module nightNdayModule.psm1 -Force
+Add-Type -TypeDefinition @'
+using System.Runtime.InteropServices;
+[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDevice {
+    int a(); int o();
+    int GetId([MarshalAs(UnmanagedType.LPWStr)] out string id);
+}
+[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDeviceEnumerator {
+    int f();
+    int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+}
+[ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }
+public class Audio {
+    public static string GetDefault (int direction) {
+    var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+    IMMDevice dev = null;
+    Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(direction, 1, out dev));
+    string id = null;
+    Marshal.ThrowExceptionForHR(dev.GetId(out id));
+    return id;
     }
 }
-#$regPath = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount\$$windows.data.bluelightreduction.settings\Current'
-#$nightSetting = ([byte[]](2,0,0,0,199,91,231,198,81,107,210,1,0,0,0,0,67,66,1,0,2,1,202,20,14,21,0,202,30,14,7,0,207,40,208,15,202,50,14,16,46,49,0,202,60,14,8,46,47,0,0))
-#$daySetting = ([byte[]](0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
-function set-nightlight(){
-    param(
-        [ValidateSet("day","night")] $mode = 'day'
-    )
-    $editingName = 'Data'
-    $baseData = @{
-        #RegPath = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount\$$windows.data.bluelightreduction.settings\Current'
-        RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount\$$windows.data.bluelightreduction.bluelightreductionstate\Current'
-        nightSetting = ([byte[]](2,0,0,0,199,91,231,198,81,107,210,1,0,0,0,0,67,66,1,0,2,1,202,20,14,21,0,202,30,14,7,0,207,40,208,15,202,50,14,16,46,49,0,202,60,14,8,46,47,0,0))
-        daySetting = ([byte[]](0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
-    }
-    if($mode -eq 'night'){
-        Set-ItemProperty -Path $baseData.regPath -Name $editingName -Value $baseData.nightSetting
-    }
-    elseif($mode -eq 'day'){
-        Set-ItemProperty -Path $baseData.regPath -Name $editingName -Value $baseData.daySetting
-    }
-    else{Write-Error "No affect made"}
-}
-function set-NightMode($recordPath = '.\ancillary_files\record.txt'){
-    #$nightSetting = ([byte[]](2,0,0,0,199,91,231,198,81,107,210,1,0,0,0,0,67,66,1,0,2,1,202,20,14,21,0,202,30,14,7,0,207,40,208,15,202,50,14,16,46,49,0,202,60,14,8,46,47,0,0))
-    Start-Process -FilePath '.\ancillary_files\night.bat' -WindowStyle Hidden #switches projection
+'@
 
-    Start-Sleep -Seconds 3
-    #set-nightlight -mode 'night' #sets nightlight on
-    Copy-Item -Path .\ancillary_files\nightSettings.json -Destination .\ancillary_files\dimmer\Dimmer.json -Force
-    while($null -eq (Get-Process -Name *dimmer*)){
-        Invoke-Item ".\ancillary_files\dimmer\Dimmer.exe"
-    }
-    Set-Content -Path $recordPath -Value "night"
-}
-function set-DayMode($recordPath = '.\ancillary_files\record.txt'){
-    #$baseData.daySetting = ([byte[]](0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
-    Start-Process -FilePath '.\ancillary_files\day.bat' -WindowStyle Hidden #switches projection
-    Start-Sleep -Seconds 3
-    #set-nightlight -mode 'day' #sets nightlight off
-    get-process -Name 'Dimmer'|<#Where-Object{$_.Path -eq ".\ancillary_files\dimmer\Dimmer.exe"}|#>Stop-Process
-    Set-Content -Path $recordPath -Value "day"
-}
-function switch-mode($recordPath = '.\ancillary_files\record.txt'){
-    $record = get-record -path $recordPath
-
-    if($record -eq "day"){
-        set-NightMode -recordPath $recordPath
-        return "night"
-    }
-    elseif($record -eq "night"){
-        set-DayMode -recordPath $recordPath
-        return "day"
-    }
-    else{
-        set-NightMode -recordPath $recordPath
-        return 'night'
-    }
-}
 #set-nightlight -mode Night
 #$record = get-record
 #$record
